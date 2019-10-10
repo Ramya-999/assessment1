2281f3debdc958f7b29de85c5a5179f513ddf010
Ramya-999 <38486111+Ramya-999@users.noreply.github.com>
2019-10-10 16:01:20 +0530

    Update Bankaccount.go

--- Bankaccount.go
+++ Bankaccount.go
@@ -7,20 +7,58 @@ type bank struct{
     name,acctype string
 }
 
+func  create() {
+	b := bank{}
 
-
-func (b *bank) deposit() float32{
-	fmt.Println("enter money to be deposited")
-    var x int
-    fmt.Scan(&x)
+	fmt.Println("enter the name: ")
+	fmt.Scan(&b.name)
+	fmt.Println("enter the account type: ")
+	fmt.Scan(&b.acctype)
+	fmt.Println("enter the account no: ")
+	fmt.Scan(&b.acc_no)
+	fmt.Println("enter the balance: ")
+	fmt.Scan(&b.bal)
 	
+    fmt.Println()
+
+    fmt.Println("Account no: ", b.acc_no)
+	fmt.Println("Name: ", b.name)
+	fmt.Println("Account type: ", b.acctype)
+	fmt.Println("Balance: ", b.bal)
+	for{
+	fmt.Println("To deposit money enter 1 -  To withdraw money enter 2 - To check the balance enter 3 - To exit enter 4")
+	var c int
+	fmt.Scan(&c)
+	if c == 4{
+		fmt.Println("Exited")
+		break
+	}
+	switch c {
+	case 1:
+		fmt.Println("enter money to be deposited")
+        var x int
+        fmt.Scan(&x)
+		fmt.Println("deposited: ", b.deposit(x))
+	case 2:
+		fmt.Println("enter money to be withdrawn")
+	    var y int
+	    fmt.Scan(&y)
+		fmt.Println("remaining balance: ", b.withdraw(y))
+	case 3:
+		fmt.Println("Available Balance: ", b.check())	
+	default:
+		fmt.Println("enter a valid number")
+
+	}
+    }
+	
+}
+
+func (b *bank) deposit(x int) float32{	
 	b.bal += float32(x)
   	return b.bal
 }
-func (b *bank) withdraw() float32 {
-	fmt.Println("enter money to be withdrawn")
-	var y int
-	fmt.Scan(&y)
+func (b *bank) withdraw(y int) float32 {
 	
 	if float32(y) > b.bal {
 		fmt.Println("not enough balance")
@@ -34,19 +72,6 @@ func (b *bank) check() float32 {
 }
 
 func main(){
-	b := bank { acc_no: 50101, bal: 40000, name: "Maya", acctype:"salary"}
-	
-	fmt.Println("Account no: ", b.acc_no)
-	fmt.Println("Name: ", b.name)
-	fmt.Println("Account type: ", b.acctype)
-	fmt.Println("Balance: ", b.bal)
-
-	
-		fmt.Println("deposited: ", b.deposit())
-	
-		fmt.Println("remaining balance: ", b.withdraw())
-		
-		fmt.Println("Available Balance: ", b.check())
-		
+		create()	
 	}
 	
